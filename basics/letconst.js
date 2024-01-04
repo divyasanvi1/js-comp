7d7614231726a511a00de0f24834ddfb676d0fde
divyasanvi1 <108477155+divyasanvi1@users.noreply.github.com>
2024-01-04 13:10:43 +0000

    letconst

---
+++ basics/letconst.js
@@ -0,0 +1,11 @@
+const accountId=12234;
+let accountName="divya";
+var accountCity="nawada";
+let accountState;
+// if no values is given then the output will be undefined
+accountName="sanvi";
+accountCity="bengaluru";
+//const cannot be changed
+//var and let is same and their values can be changed
+// var is now not in use becuse it has issue with scope that is block scope and functional scope
+console.table([accountId,accountName, accountCity, accountState]);
