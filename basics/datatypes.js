88c1aaf4c9b5886403faaa1f4b0519ed23a34800
divyasanvi1 <108477155+divyasanvi1@users.noreply.github.com>
2024-01-04 13:55:51 +0000

    datatype

---
+++ basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // this will treat all js code as newer version of javascript
+
+//alert(3+3) will give error as we are using node.js not browser
+let name="divya";
+let age=18
+let loggedIn= true;
+
+// datatype--
+/*
+number = 2 to power 53
+bigint = for verybig numbers
+string =""
+boolean= true/false
+null= standalone value
+undefined =value not defined
+symbol =unique
+*/
+
+//object
+
+console.log(typeof "divya");
+console.log(typeof 18);
+console.log(typeof true);
+
