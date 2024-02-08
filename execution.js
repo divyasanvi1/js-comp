96194e3ce27ebe55425fe4d62c3c8c309b48f205
divyasanvi1 <108477155+divyasanvi1@users.noreply.github.com>
2024-02-08 07:20:06 +0000

    final

---
+++ execution.js
@@ -0,0 +1,11 @@
+// javascript execution context-how javascript will run the js code
+// javascript run the program or code in two phases
+// there will always be a global execution context and this variable refers global execution contextevery environmenthas its 
+// own value for this like in browsers this has value == window object ,node js environment has different this value.
+// these execute inside a thread
+// -global execution context
+// -function execution context
+// -eval execution context --- property of global object
+// javascript run the program or code in two phases
+// --memory creation phase
+// --execution phase
