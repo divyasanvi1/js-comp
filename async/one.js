4ca07b0bbb372e4b874dfa786e6dd863a917977e
divyasanvi1 <108477155+divyasanvi1@users.noreply.github.com>
2024-02-15 11:52:50 +0000

    one

---
+++ async/one.js
@@ -0,0 +1,6 @@
+//default javascript -synchronous and single threaded
+//execution context -each operation waits for the last one to complete before executing
+// js engine is single threaded so it never comes standalone it  should must come with a runtime environment
+
+// blocking code -block the flow of program- read file sync
+//non blocking code - donot block the flow of program -read file async
